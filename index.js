7279a724118eb10d63187d6c412aa95c3e29911b
María García <mariav2601@gmail.com>
2025-04-04 10:44:15 -0400

    Agregar función para capturar una captura de pantalla y ajustar la velocidad de Puppeteer

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import puppeteer from "puppeteer";
 async function openWebPage() {
     const browser = await puppeteer.launch({
     headless: false,
-    slowMo: 200,
+    slowMo: 400,
 });
 const page = await browser.newPage();
 await page.goto("https://multimax.com.ve/");
@@ -11,3 +11,14 @@ await browser.close();
 }
 
 //openWebPage();
+
+async function captureScreenshot() {
+    const browser = await puppeteer.launch({
+    headless: false,
+    slowMo: 400,
+});
+const page = await browser.newPage();
+await page.goto("https://multimax.com.ve/");
+await page.screenshot({ path: "example.png", fullPage: true });
+await browser.close();
+}
